91265cc62bd98991c1a7c4f50513e76aecca917c
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2022-03-26 19:06:19 -0700

    Update Scope.js

--- Scope.js
+++ Scope.js
@@ -13,3 +13,9 @@ inner();
 scope()
 
 // scope() returns 10 20 30
+
+/*
+1. Lexical refers to the fact that lexical scoping uses the location where a variable is declared within the source code to determine where that variable is available
+2. In case of nested function, the JS engine looks for the innermost function and moves outward until it reaches global scope.
+3. Nested functions have access to variables declared in their scope as well as the variables declared in outer scope.
+*/
