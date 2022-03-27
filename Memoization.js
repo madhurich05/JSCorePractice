75df7655459d613ac50bcf9ed227b9bf024a80fa
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2022-03-26 23:12:22 -0700

    Update Memoization.js

--- Memoization.js
+++ Memoization.js
@@ -23,8 +23,33 @@ const sayHiOnce = once(sayHi) // return sayHi
 console.log(sayHiOnce('Bob'))  // hi Bob
 console.log(sayHiOnce('Bill')) // hi Bob
 
-// const add = (a,b) => a + b
-// const addOnce = once(add)
+------------------------------------------------------------------------------------------------------------------------------------
 
-// console.log(addOnce(-4, 4))    // 14
-// console.log(addOnce(210, 210)) // 14
+// Generic memoize function
+
+function memoize (callback) {
+  let cache = {};
+  
+  return function (...args) {
+    const key = args.toString();
+    
+    if(key in cache) {
+      console.log('Returning from Cache');
+     return cache[key]; 
+    }  else {
+      const result = callback(...args);
+      cache[key] = result;
+      return result;
+    }    
+  }
+}
+
+
+function add(a,b) {
+  return a+b;
+}
+
+const memoizedAdd = memoize(add);
+console.log(memoizedAdd(2,3));  // 5
+console.log(memoizedAdd(2,3));
+
