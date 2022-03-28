040e706d820b880787669ee4ace050852b5dc046
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2022-03-28 09:17:14 -0700

    Update CustomBind.js

--- CustomBind.js
+++ CustomBind.js
@@ -3,3 +3,28 @@ function bind(fn, context) { // Bind is a function that takes fn and context
     fn.apply(context); // returns function after applying the context
   }
 }
+
+
+// Bind method with arguments
+
+function bind(fn, context) { // Bind is a function that takes fn and context
+  return function(){ // Bind returns a function
+    fn.apply(context,[...arguments]); // returns function after applying the context
+  }
+}
+
+
+// Try the custom bind
+
+const Person = {
+  name: 'Madhuri'  
+}
+
+function greeting(lastName) {
+    console.log(`Hello ${this.name} ${lastName}`)
+  }
+
+const boundFn = bind(greeting, Person); // Using Bind with arguments function
+
+boundFn('Chadalapaka')
+//Hello Madhuri Chadalapaka
