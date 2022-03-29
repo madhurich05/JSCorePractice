57f7b06867b5e55d226dc46be5d145e2cc84365c
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2022-03-29 12:20:59 -0700

    Update GenericCurryFn.js

--- GenericCurryFn.js
+++ GenericCurryFn.js
@@ -1,3 +1,22 @@
-function generalCurryOne() {
-  
+// Not working as expected
+
+function generalCurryOne(f) {
+  return function currify(...args) {
+    if(args.length >= f.length) { // If we have all the args the original function needs
+      return f.apply(this, args);   //Use explicit binding 
+  } else {
+    return function(...newArgs) {
+      return currify(this, args.concat(newArgs)); // call currify again after concatinating the args with new args
+    }
+  }
 }
+}
+
+ 
+  function sum(a,b) {
+    return a+b;    
+  }
+  
+  let curriedSum = generalCurryOne(sum);
+  curriedSum(2,3);
+  curriedSum(2)(3);
