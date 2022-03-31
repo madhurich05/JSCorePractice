1d0e8c78042dd8d36ab864c07515edb360e644f9
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2022-03-31 15:32:45 -0700

    Update ES6Class.js

--- ES6Class.js
+++ ES6Class.js
@@ -1,29 +1,3 @@
-function Person(name) {
-  this.name = name;
-}
-
-Person.prototype.greeting = function () {
-  console.log(`Hello ${this.name}`);
-}
-
-Person.prototype.eat = function (food) {
-  console.log(`I love eating samosa`);
-}
-
-
-class Programmer extends Person {
-  constructor(name, language) {
-    super(name);
-    this.language = language;
-  }
-  code() {
-    console.log(`I am codinng in ${this.language}`);
-  }
-}
-------------------------------------------------------------------------------------------------------------------------------------------------------
-
-// ES6 Classes Exercise
-
 class Calculator {
 constructor() {
   this.value = 0;
