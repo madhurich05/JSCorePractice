1f7f6cb7daaecd4015d3ee0c170c360bb9aa7e3e
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2022-04-30 19:17:49 -0700

    Update LowestCommonAncestor.js

--- src/LowestCommonAncestor.js
+++ src/LowestCommonAncestor.js
@@ -4,12 +4,9 @@ var lowestCommonAncestor = function(root, p, q) {
     if(!root) {
         return null;
     }
-	// recursive call to left and right child of each node
     const left = lowestCommonAncestor(root.left, p, q),
           right = lowestCommonAncestor(root.right, p, q);
     
-	// if either both left and right child return a node or the root node value matches p or q node value,
-	// return the node
     if((left && right) || root.val === p.val || root.val === q.val) {
         return root;
     } else if(left) {    // if only one of the p or q node is found at the left child, return that
