097f2105b5550eacc6380970a0f7ddbd53510cd1
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2022-04-30 19:20:40 -0700

    Update LowestCommonAncestor.js

--- src/LowestCommonAncestor.js
+++ src/LowestCommonAncestor.js
@@ -1,18 +1,28 @@
 Lowest Common Ancestor
 
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
 var lowestCommonAncestor = function(root, p, q) {
-    if(!root) {
-        return null;
-    }
-    const left = lowestCommonAncestor(root.left, p, q),
-          right = lowestCommonAncestor(root.right, p, q);
     
-    if((left && right) || root.val === p.val || root.val === q.val) {
-        return root;
-    } else if(left) {    // if only one of the p or q node is found at the left child, return that
-        return left;
-    } else if(right) {    // if only one of the p or q node is found at the right child, return that
-        return right;
-    }
-    return null;    // none of the nodes is found when this node is processed completely, so return null
+    if(!root) return null;
+    
+    const left = lowestCommonAncestor(root.left, p, q),
+          right = lowestCommonAncestor(root.right, p,q);
+    
+    if ((left && right) || (root.val === p.val) || (root.val === q.val)) return root;
+        
+    return left ?? right;
+    return null;        
+    
 };
